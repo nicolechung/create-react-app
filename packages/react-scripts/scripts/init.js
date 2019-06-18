acef40076e743529206316df4182e106cbbdf457
Nicole Chung <nicole.chung@gmail.com>
2019-06-18 15:42:28 -0400

    changing version for semver

--- packages/react-scripts/scripts/init.js
+++ packages/react-scripts/scripts/init.js
@@ -95,10 +95,10 @@ module.exports = function(
 
   // Setup the script rules
   appPackage.scripts = {
-    start: 'everlong-react-scripts start',
-    build: 'everlong-react-scripts build',
-    test: 'everlong-react-scripts test',
-    eject: 'everlong-react-scripts eject',
+    start: 'react-scripts start',
+    build: 'react-scripts build',
+    test: 'react-scripts test',
+    eject: 'react-scripts eject',
   };
 
   // Setup the eslint config
